8092cc6d1d8f1cc87e0c03a27051480d8d476741
NiZiL <biasutto.t@gmail.com>
2014-12-06 12:09:28 +0100

    Setup

---
+++ desktop/src/org/marvellous/factory/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.marvellous.factory.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.marvellous.factory.TheMarvellousFactory;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheMarvellousFactory(), config);
+	}
+}
---
+++ html/src/org/marvellous/factory/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.marvellous.factory.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.marvellous.factory.TheMarvellousFactory;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TheMarvellousFactory();
+        }
+}
