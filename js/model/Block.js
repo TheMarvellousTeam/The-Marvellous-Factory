e555b083d76ffe20637f55dc37219b3009db7092
Platane <arthur@arthur-brongniart.fr>
2014-12-06 16:30:41 +0100

    getSymbol impl

--- js/model/Block.js
+++ js/model/Block.js
@@ -1,7 +1,12 @@
 var Abstract = require('../utils/Abstract')
 
-var getSymbols = function(){
-    return []
+var getSymbols = function( symbol ){
+    var points = [];
+    for(var x=this.shapes.length; x--; )
+    for(var y=this.shapes.length; y--; )
+        if( this.shapes[x][y] == symbol )
+            points.push({x:x, y:y})
+    return points
 }
 
 module.exports = Object.create( Abstract )
