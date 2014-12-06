ebd302a33d64449a4e10eedf3174eb60e37ca846
NiZiL <biasutto.t@gmail.com>
2014-12-06 18:51:50 +0100

    Pipe implementation

--- js/model/pipe.js
+++ js/model/pipe.js
@@ -1,7 +1,47 @@
 var Abstract = require('../utils/Abstract')
 
+var bufferSize = function(type) {
+	switch(type) {
+		default:
+			return 1;
+	}
+}
+
+var production = function(type) {
+	switch(type){
+		default:
+
+	}
+}
+
 module.exports = Object.create( Abstract )
-.extend(notifier)
 .extend({
-    
+
+	init: function(type) {
+		this.type = type ;
+		this.bufferSize = 1 ;
+	},
+
+	addInput: function(input) {
+		this.input.push(input);
+	},
+
+	addOutput: function(output) {
+		this.output.push(output);
+	},
+
+	getBufferSize: function(){
+		return bufferSize(this.type);
+	}
+
+	consume: function(){
+		for(var i=this.input.length; i-- ;) {
+			this.buffer.push(this.input[i].next());
+		}
+	},
+	
+	produce: function() {
+		production.call(this, this.type);
+	},
+
 })
